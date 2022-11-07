d71ace4268108466d1b909d46b300efd9c006256
Rafaelbb27 <rafaelbiriba27@gmail.com>
2022-11-07 12:39:44 -0300

    specified port

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-
+import _tkinter
 from .database import Base
 
 class Post(Base):
