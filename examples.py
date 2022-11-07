1dd277ad092910805bc2fdc638fcce9e8330bede
Rafaelbb27 <rafaelbiriba27@gmail.com>
2022-11-07 11:03:36 -0300

    tirando um espaco do procfile

---
+++ examples.py
@@ -0,0 +1,12 @@
+[
+    {
+        "Post": {
+            "content": "check out those awesome beaches",
+            "title": "Bye to funland",
+            "created_at": "2022-10-27T17:25:37.180115-03:00",
+            "published": true,
+            "id": 22,
+            "owner_id": 14
+        },
+        "votes": 1
+    }
