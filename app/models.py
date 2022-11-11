ba5134ed61a1c28b4c9a84eae44498ef19c2dc74
Rafaelbb27 <rafaelbiriba27@gmail.com>
2022-11-11 15:48:53 -0300

    add _tkinter CASCADE

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-from tkinter import CASCADE
+#from tkinter import CASCADE
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-import _tkinter
+from _tkinter import CASCADE
 from .database import Base
 
 class Post(Base):
